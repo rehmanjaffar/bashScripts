9106e06b96b85d4b57998c4775c280f35d813528
rehmanjaffar <142385806+rehmanjaffar@users.noreply.github.com>
2023-09-27 14:20:06 +0500

    Update docker_auth.sh

--- docker_private_registry/docker_auth.sh
+++ docker_private_registry/docker_auth.sh
@@ -30,3 +30,48 @@ fi
 # Step 2: Generate the htpasswd file for authentication
 docker run --rm --entrypoint htpasswd httpd:2.4 -Bbn $USERNAME $PASSWORD > $AUTH_PATH/htpasswd
 # Step 3: Stop and remove the existing registry container if it's running
+if docker ps -a | grep -q "local.hub"; then
+    docker stop local.hub
+    docker rm local.hub
+fi
+
+# Step 4: Run the Docker registry with SSL certificates and basic authentication
+docker run -d \
+  -p 443:443 \
+  --name local.hub \
+  -v $CERTS_PATH:/certs \
+  -v $REGISTRY_STORAGE_PATH:/var/lib/registry \
+  -v $AUTH_PATH:/auth \
+  -e "REGISTRY_AUTH=htpasswd" \
+  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+  -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+  -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/localhub.crt \
+  -e REGISTRY_HTTP_TLS_KEY=/certs/localhub.key \
+  registry
+
+# Step 5: Ensure the registry container is up and running
+docker container ls | grep local.hub
+
+# Step 6: Login to the private registry
+docker login $DOMAIN_NAME
+
+# Note: At this point, you'll be prompted to enter the username and password. After successful authentication, you can push and
+# pull images from the registry.
+docker pull $IMAGE_TO_PULL
+# Step 7: Tag the Alpine image for the local registry
+docker tag $IMAGE_TO_PULL $LOCAL_IMAGE_NAME
+# Step 8: Push the image to the local registry
+docker push $LOCAL_IMAGE_NAME
+# Step 9: Remove containers using the Alpine image
+docker ps -a | grep $IMAGE_TO_PULL | awk '{print $1}' | xargs docker rm -f
+# Step 10: Remove the Alpine and its tagged version
+docker rmi $LOCAL_IMAGE_NAME
+docker rmi $IMAGE_TO_PULL
+# Step 11: Pull the image from the local registry
+docker pull $LOCAL_IMAGE_NAME
+# Step 12: Run a container with the pulled image
+docker run $LOCAL_IMAGE_NAME ls
+
+
+echo "Setup and verification complete!"
